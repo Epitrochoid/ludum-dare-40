ee51323041661070000dbcb1b46914d1958a5f7a
James Beavers <jamesjbeavers@gmail.com>
2017-12-02 17:32:34 -0500

    change default render location/window sizing - stretch, keep ratio. Add linedit

---
+++ LineEdit.gd
@@ -0,0 +1,17 @@
+extends LineEdit
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	append_at_cursor('> ')
+	pass
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+#	append_at_cursor('hello world')
+	pass
