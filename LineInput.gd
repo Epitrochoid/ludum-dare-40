7964e62e3b4a502b21d5a250276303be8ccc1852
James Beavers <jamesjbeavers@gmail.com>
2017-12-03 12:08:48 -0500

    Terminal scrolling. More work to do, but some progress at least

--- LineInput.gd
+++ LineInput.gd
@@ -7,8 +7,16 @@ extends LineEdit
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	append_at_cursor('> ')
+	grab_focus()
+	self.connect("text_entered", self, "_on_enter_pressed")
 	pass
+
+func _on_enter_pressed(someText):
+	print('input received! ', someText)
+	var terminal_node = self.get_parent().get_parent()
+	var new_terminal_line = load("res://TerminalLine.tscn").instance()
+	terminal_node.add_child(new_terminal_line)
+
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
