1c93bd712156509575b086897f68bf2b39028ca5
Charlie Duong <charlieduong94@gmail.com>
2017-12-03 18:34:06 -0500

    Make tasks great again

--- Tasks.gd
+++ Tasks.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Container
 
 # class member variables go here, for example:
 # var a = 2
@@ -7,12 +7,12 @@ extends Control
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-    var addTaskTimer = Timer.new()                                                                    
-    addTaskTimer.set_wait_time(5)                                                                     
-    addTaskTimer.connect("timeout", self, "_on_add_task_timeout")                                     
+    var addTaskTimer = Timer.new()
+    addTaskTimer.set_wait_time(1)
+    addTaskTimer.connect("timeout", self, "_on_add_task_timeout")
     addTaskTimer.set_one_shot(false)
-    add_child(addTaskTimer)                                                                           
-    addTaskTimer.start()                                                                              
+    add_child(addTaskTimer)
+    addTaskTimer.start()
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -22,5 +22,5 @@ func _ready():
 func _on_add_task_timeout():
   var test_label = Label.new()
   test_label.set_text("Test")
-  add_child(test_label)
+  get_parent().add_child(test_label)
   print("Timeout hit")
